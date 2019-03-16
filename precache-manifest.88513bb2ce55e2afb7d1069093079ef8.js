d74217719fd17118dbb2d710aa4dc49c2b373217
bareseph <barryriveroll@gmail.com>
2019-03-16 18:47:58 -0400

    Updates

---
+++ precache-manifest.88513bb2ce55e2afb7d1069093079ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "484c2cc1c67a6ce4e33b",
+    "url": "/react-clicky-game/static/js/main.f40ff584.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-clicky-game/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "484c2cc1c67a6ce4e33b",
+    "url": "/react-clicky-game/static/css/main.2bec49a2.chunk.css"
+  },
+  {
+    "revision": "40a3184c1eb31eec8cb440d6ac6cdc77",
+    "url": "/react-clicky-game/index.html"
+  }
+];
